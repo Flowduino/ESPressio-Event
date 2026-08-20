e8a8216cbfe303eb1879c25ad88bd2d77f2b6ea3
Simon J Stuart <lakraven@gmail.com>
2026-08-20 19:39:42 +0200

    refactor: keep socket Event families independently includable

--- src/ESPressio_SocketEvents.hpp
+++ src/ESPressio_SocketEvents.hpp
@@ -1,33 +1,4 @@
 #pragma once
 
-#include <string>
-
-#include <ESPressio_SecurityTypes.hpp>
-
-#include "ESPressio_Event.hpp"
-
-namespace ESPressio::Event {
-
-class SocketWorkerStartedEvent final : public Event<> {
-public:
-    const std::string Name;
-    explicit SocketWorkerStartedEvent(const char* name) : Name(name == nullptr ? "" : name) {}
-};
-
-class SocketWorkerStartFailedEvent final : public Event<> {
-public:
-    const std::string Name;
-    explicit SocketWorkerStartFailedEvent(const char* name) : Name(name == nullptr ? "" : name) {}
-};
-
-class SocketWorkerStoppedEvent final : public Event<> {};
-
-class SocketSecuritySessionFaultedEvent final : public Event<> {
-public:
-    const Security::SecurityResult Result;
-    explicit SocketSecuritySessionFaultedEvent(const Security::SecurityResult& result) : Result(result) {}
-};
-
-class SocketSecuritySessionResetEvent final : public Event<> {};
-
-} // namespace ESPressio::Event
+#include "ESPressio_SocketWorkerEvents.hpp"
+#include "ESPressio_SocketSecuritySessionEvents.hpp"
